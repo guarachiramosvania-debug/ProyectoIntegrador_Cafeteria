1440a9a28debfe2f1a424aa54c5ebf456b058258
ricardoNP51 <58277964+ricardoNP51@users.noreply.github.com>
2025-11-16 17:58:07 -0400

    Delete Datos directory

--- Datos/Repositorios/PedidoRepository.cs
+++
@@ -1,8 +0,0 @@
-using ProyectoIntegrador_Cafeteria.Negocio.Modelos;
-
-namespace ProyectoIntegrador_Cafeteria.Datos.Repositorios
-{
-    public class PedidoRepository : BaseRepository<Producto>
-    {
-    }
-}
