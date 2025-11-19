ed464c95bcb65d963e8a433a1a14fd1b5a82d75a
ricardoNP51 <r_nunezdelprado@icloud.com>
2025-11-19 11:24:07 -0400

    csmbios

--- CoffeTime/CoffeTime/Presentacion/Views/ProveedoresView.xaml.cs
+++ CoffeTime/CoffeTime/Presentacion/Views/ProveedoresView.xaml.cs
@@ -17,11 +17,18 @@ namespace CoffeTime.Presentacion.Views
     /// <summary>
     /// Interaction logic for ProveedoresView.xaml
     /// </summary>
-    public partial class ProveedoresView : Window
+    public partial class ProveedoresView : UserControl
     {
         public ProveedoresView()
         {
             InitializeComponent();
+
+            ListaProveedores.ItemsSource = new List<dynamic>
+        {
+            new { Nombre="Café Premium S.A.", ContactoNombre="Juan Pérez", Email="ventas@cafepremium.com", Telefono="+1234567890", FechaRegistro="31/12/2024" },
+            new { Nombre="Lácteos del Valle", ContactoNombre="Ana García", Email="pedidos@lacteosvalle.com", Telefono="+1234567891", FechaRegistro="31/12/2024" },
+            new { Nombre="Dulces y Postres", ContactoNombre="Roberto Sánchez", Email="contacto@dulcesypostres.com", Telefono="+1234567892", FechaRegistro="31/12/2024" }
+        };
         }
     }
 }
