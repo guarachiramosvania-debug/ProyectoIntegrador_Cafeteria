081d8fb9bb697757c165d14f40a7578118f270da
Jose Andres Huamani Huaypuna <hhj0035958@est.univalle.edu>
2025-11-19 10:29:18 -0400

    insumos y pedidos

--- CoffeTime/CoffeTime/Datos/Repositorios/PedidoRepository.cs
+++ CoffeTime/CoffeTime/Datos/Repositorios/PedidoRepository.cs
@@ -1,8 +1,59 @@
 using ProyectoIntegrador_Cafeteria.Negocio.Modelos;
+using Supabase;
 
-namespace ProyectoIntegrador_Cafeteria.Datos.Repositorios
+public class PedidoRepository
 {
-    public class PedidoRepository : BaseRepository<Producto>
+    private readonly Supabase.Client _client;
+
+    public PedidoRepository(Supabase.Client client)
     {
+        _client = client;
+    }
+
+    public async Task<long> CrearPedido(Pedido pedido)
+    {
+        var result = await _client
+            .From<Pedido>()
+            .Insert(pedido);
+
+        return result.Models[0].IdPedido;
+    }
+
+    public async Task<Pedido> ObtenerPedido(long idPedido)
+    {
+        var result = await _client
+            .From<Pedido>()
+            .Where(p => p.IdPedido == idPedido)
+            .Single();
+
+        return result;
+    }
+
+    public async Task<List<Pedido>> ObtenerTodos()
+    {
+        var result = await _client
+            .From<Pedido>()
+            .Get();
+
+        return result.Models;
+    }
+
+    // LA FUNCIÓN 100% FUNCIONAL
+    public async Task ActualizarEstado(long idPedido, string nuevoEstado)
+    {
+        var pedido = await _client
+            .From<Pedido>()
+            .Where(p => p.IdPedido == idPedido)
+            .Single();
+
+        if (pedido == null)
+            throw new Exception("El pedido no existe.");
+
+        pedido.Estado = nuevoEstado;
+
+        await _client
+            .From<Pedido>()
+            .Where(p => p.IdPedido == idPedido)
+            .Update(pedido);
     }
 }
